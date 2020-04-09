15463e04151b4a84b4bd9fc2183d0cc641e3174b
Samuria86 <60991886+Samuria86@users.noreply.github.com>
2020-04-08 19:26:13 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+(function () {
+  'use strict';
+  angular.module('ShoppingListApp', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+      var toBuy = this;
+
+      toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+      toBuy.remove = function (itemIndex) {
+        ShoppingListCheckOffService.remove(itemIndex);
+      };
+
+      toBuy.message = function () {
+  			return (toBuy.items == "");
+  		};
+    }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var alreadyBought = this;
+
+    alreadyBought.items = ShoppingListCheckOffService.itemsAlreadyBought;
+
+    alreadyBought.message = function () {
+			return (alreadyBought.items == "");
+		};
+
+  };
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    // List of shopping items
+    service.itemsToBuy = [];
+    service.itemsAlreadyBought = [];
+
+    service.itemList = [
+      {name:"cookies", quantity:"10"},
+      {name:"soda", quantity:"10"},
+      {name:"milk", quantity:"10"},
+      {name:"bread", quantity:"10"},
+      {name:"cookies", quantity:"10"}
+    ];
+
+    service.getItemsToBuy = function () {
+      service.itemsToBuy = service.itemList;
+      return service.itemsToBuy;
+    };
+
+    service.remove = function (itemIndex) {
+			var x = service.itemsToBuy.splice(itemIndex, 1)[0];
+			service.itemsAlreadyBought.push(x);
+		};
+
+    service.getItemsAlreadyBought = function () {
+      return itemsAlreadyBought;
+    };
+  }
+
+})();
